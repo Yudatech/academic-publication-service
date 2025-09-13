9ce6e5d8c6b7d0ea3f170879bb540ac44991085b
Hui Su <yudasuhui@hotmail.com>
2025-09-14 00:47:14 +0200

    move componets to folders

--- src/routes/SearchPage.tsx
+++ src/routes/SearchPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import SearchBar from "@/components/SearchBar";
-import InfiniteList from "@/components/InfinityList";
+import InfiniteList from "@/components/search/InfinityList";
 
 const MIN_LEN = 2;
 
